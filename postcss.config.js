99cdbe696a4b01d16180fbe4ba83f73716c1b9ad
Damar Galih AJi Pradana <108448450+D4marp@users.noreply.github.com>
2025-12-23 20:17:29 +0700

    Fix postcss config and next.config for production deployment

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
-  plugins: [require('tailwindcss'), require('autoprefixer')],
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
-
-export default config
